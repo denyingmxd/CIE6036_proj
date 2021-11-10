e99ca192fa217eb009c60dbe8b79f7a0fd8f15d5
denyingmxd <117010053@link.cuhk.edu>
2021-11-10 14:44:11 +0800

    price-flowrestrict

--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import numpy as np
 from utils import orginal_utility,allocate_1,\
-    max_allocatin,min_allocation,optimal_allocation
+    max_allocatin,min_allocation,optimal_allocation,pricing_allocation,\
+    flow_restrict_allocation
 import matplotlib.pyplot as plt
 
 
@@ -56,35 +57,73 @@ def compare_SWs_and_SWp():
     H_0 = 251.5
     h = 30
     A = 100
-    theta = 4
+    theta = 0.001
     b = 0.001
     d = 0.01
     alphas = np.arange(0.0,0.85,0.01)
     max_ress=[]
     optimal_ress=[]
+    pricing_ress=[]
     for alpha in alphas:
         max_res = max_allocatin(N, H_0*(1-alpha), h, A, theta, b,d)
-        optimal_res = optimal_allocation(N, H_0, h, A, theta, b,d,alpha)
-        # print(max_res)
-        # print(optimal_res)
+        optimal_res = optimal_allocation(N, H_0*(1-alpha), h, A, theta, b,d)
+        pricing_res = pricing_allocation(N, H_0*(1-alpha), h, A, theta, b,d,p=0.1)
+        print(max_res)
+        print(optimal_res)
+        print(pricing_res)
         # exit()
-
+        pricing_ress.append(pricing_res)
         max_ress.append(max_res)
         optimal_ress.append(optimal_res)
     max_ress=np.array(max_ress)
     optimal_ress=np.array(optimal_ress)
     plt.plot(alphas,max_ress/optimal_ress)
-    print(max_ress/optimal_ress)
-    # plt.plot(alphas,optimal_res)
+    plt.plot(alphas,pricing_ress/optimal_ress)
     plt.show()
     return 0
 
+
+def test_flow_restrict_allocation():
+    N=3
+    H_0=118
+    h=30
+    A=100
+    theta=4
+    b=0.2
+    d=1
+    cs=np.arange(0.2,1,0.01)
+    rr=[]
+    for c in cs:
+        res = flow_restrict_allocation(N, H_0, h, A, theta, b,d,c)
+        rr.append(res)
+    plt.plot(cs,rr)
+    plt.show()
+    return
+
+
+
+def test_supply_restrict_allocation():
+    N=10
+    A=100
+    H_0=131
+    h=30
+    b=0.01
+    d=0.1
+    theta1=1
+    theta2s=np.arange(1,100,1)
+    for theta2 in theta2s:
+        pass
 def main():
 
     # test_max_allocation()
     # test_min_allocation()
     # test_optimal_allocation()
-    compare_SWs_and_SWp()
+
+    # compare_SWs_and_SWp()
+
+    # test_flow_restrict_allocation()
+    test_supply_restrict_allocation()
+
     pass
 
 main()
--- utils.py
+++ utils.py
@@ -31,18 +31,64 @@ def get_n_b(N,A,b,h,H_0):
     xx = 3 * (H_0 - h) / (2 * A * b ** 2) + 1 / 2 * np.sqrt((9 * (H_0 - h) ** 2) / (A ** 2 * b ** 4) - 1 / 432)
     yy = 3 * (H_0 - h) / (2 * A * b ** 2) - 1 / 2 * np.sqrt((9 * (H_0 - h) ** 2) / (A ** 2 * b ** 4) - 1 / 432)
     y_b = xx ** (1 / 3) + yy ** (1 / 3) - 1 / 2
-    print(upper)
-    if lower<=H_0:
-        print(y_b)
+    # print(upper)
+    if lower<=H_0 and H_0<=upper:
+        # print(y_b)
         return np.floor(y_b)
     if H_0<lower:
         return 0
-    else:
+    if H_0>upper:
         return N
 
 
-def allocate_1():
+def allocate_1(N,H_0,A,xs):
     pass
+
+
+
+def I_2(q_d,b):
+    if q_d<b:
+        return 0
+    else:
+        return 1
+
+
+
+def pricing_allocation(N,H_0,h,A,theta,b,d,p):
+    n_b=get_n_b(N,A,b,h,H_0)
+    n_d =get_nd(N,A,b,d,h,H_0)
+    print(n_d,n_b)
+    q_d=-1/2*d*n_d\
+        +np.sqrt(
+        -1/3*d**2*n_d*(n_d+1)**2*(n_d+2)+4*(n_d+1)*(H_0-h)/A
+    )/(2*(n_d+1))
+    if theta<(n_b-n_d-I_2(q_d,b))/((n_d*(d-b)+max(q_d-b,0))):
+        print('pricing using max allocation and p in range [{},{}]'.format(theta,1/b))
+        SW_p=n_b
+    else:
+        print('pricing using min allocation and p in range [{},inf]'.format(theta))
+        SW_p=n_d*(1+theta*(d-b))+I_2(q_d,b)+theta*max(q_d-b,0)
+    return SW_p
+
+
+
+def flow_restrict_allocation(N, H_0, h, A, theta, b,d,c):
+    n_c = get_nd(N, A, b, c, h, H_0)
+    # print('max allocation with c:',n_c)
+    q_c = -1 / 2 * c * n_c \
+          + np.sqrt(
+        -1 / 3 * c ** 2 * n_c * (n_c + 1) ** 2 * (n_c + 2) + 4 * (n_c + 1) * (H_0 - h) / A
+    ) / (2 * (n_c + 1))
+
+    if n_c==N:
+        SW_c=n_c*(1+theta*(c-b))
+    if n_c<N and q_c<b:
+        SW_c = n_c * (1 + theta * (c - b))
+    if n_c<N and q_c>=b:
+        SW_c = SW_c=n_c*(1+theta*(c-b))+(1+theta*(q_c-b))
+    return SW_c
+
+
 
 
 def max_allocatin(N,H_0,h,A,theta,b,d):
@@ -75,9 +121,9 @@ def val_water(j,k,b,d,A,H_0,h):
 
 
 
-def optimal_allocation(N, H_0, h, A, theta, b,d,alpha):
+def optimal_allocation(N, H_0, h, A, theta, b,d):
     mm = -1
-    H_0 = H_0*(1-alpha)
+    H_0 = H_0
     for j in range(1,N+1):
         for k in range(j,N+1):
             if not val_water(j,k,b,d,A,H_0,h):
@@ -90,7 +136,6 @@ def optimal_allocation(N, H_0, h, A, theta, b,d,alpha):
 
 
 
-    pass
 
 
 def min_allocation(N, H_0, h, A, theta, b):
